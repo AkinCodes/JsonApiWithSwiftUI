b9092f9fd80771c880d3229aa7624e89e5ddb7e4
akinolussanya <66926466+akinolussanya@users.noreply.github.com>
2021-08-14 14:58:24 +0100

    Initial Commit

---
+++ JsonApiWithSwiftUI/JsonApiWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonApiWithSwiftUIApp.swift
+//  JsonApiWithSwiftUI
+//
+//  Created by Akin on 14/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct JsonApiWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
